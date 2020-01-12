b34b360ecfe086196f81a73f37365b341274c966
Meghana <meghanagangolli@gmail.com>
2020-01-12 23:47:09 +0530

    Added sections

---
+++ src/Hire.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Hire.css'
+
+const Hire = () =>{
+    return(
+        <section id="experience">
+             <div class="box" >
+                 <p class="b1"><b>HIRE ME!</b></p>
+             </div>
+    
+        </section>
+    )
+}
+export default Hire
